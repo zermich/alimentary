8f3bf1fc9443999d09cf8f8361f3cd66d8e36f06
Kristen Z <zermattenk@gmail.com>
2018-09-27 17:46:08 -0500

    Added Route to EditItem page

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import AddItem from './components/AddItem';
+import EditItem from './components/EditItem';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import IndexItem from './components/IndexItem';
 
 ReactDOM.render(
   <Router>
     <div>
-      <Route exact path='/' component={App} />
+      <Route exact path='/' component={IndexItem} />
       <Route path='/add-item' component={AddItem} />
-      <Route path='/index' component={IndexItem} />
+      <Route path='/edit/:id' component={EditItem} />
+
     </div>
   </Router>,
   document.getElementById('root'));
