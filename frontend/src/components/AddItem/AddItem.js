ec1d80feafacfd85a1527357214ce77b2ad6f741
Kristen Z <zermattenk@gmail.com>
2019-10-09 15:55:33 -0500

    Restyled AddItem to EditItem

--- frontend/src/components/AddItem/AddItem.js
+++ frontend/src/components/AddItem/AddItem.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import ItemService from '../../Service/ItemService';
 import { withRouter, Link, Redirect } from 'react-router-dom';
 import localforage from 'localforage';
-// import injectSheet from 'react-jss';
+import injectSheet from 'react-jss';
 
-// import { AddItemStyles } from './AddItem.styles';
+import { EditItemStyles } from '../EditItem/EditItem.styles';
 
 class AddItem extends Component {
 
@@ -63,22 +63,23 @@ class AddItem extends Component {
       return (<Redirect to="/login"/>);
     }
 
-    // const { classes } = this.props;
+    const { classes } = this.props;
     
     const checkUserLogin = (this.props.user === '' ? '' : `You are logged in as: ${this.props.user}` );
 
     return(
-      <div className="container">
+      <div className={ classes.editItemContainer }>
         <div>{checkUserLogin}</div>
-        <button type="submit" onClick={this.handleLogout}>Logout</button>
+        <button type="submit" onClick={this.handleLogout} className={ classes.btn }>Logout</button><br/><br/>
         <form onSubmit={this.handleSubmit}>
-          <label>
+          <label className={ classes.labels }>
             Add Item:
-            <input autoFocus type="text" name="item" value={this.state.item} onChange={this.handleChange} className="form-control" />
+            <input autoFocus type="text" name="item" value={this.state.item} onChange={this.handleChange} className={ `form-control ${classes.noZoom}`} />
           </label><br/>
-          <label>
+          <label className={ classes.labels }>
             Category:&nbsp;
-            <select name="category" onChange={this.handleChange}>
+          </label><br />
+          <select name="category" onChange={this.handleChange} className={ classes.noZoom }>
               <option value='other'>Other</option>
               <option value='bakery'>Bakery</option>
               <option value='baking'>Baking</option>
@@ -89,22 +90,24 @@ class AddItem extends Component {
               <option value='home'>Home</option>
               <option value='meat'>Meat</option>
               <option value='produce'>Produce</option>
-            </select>
-          </label><br />
-          <label>
+          </select><br/><br/>
+          <label className={ classes.labels }>
             Quantity:
-            <input type="text" name="quantity" value={this.state.quantity} onChange={this.handleChange} className="form-control" />
+            <input type="text" name="quantity" value={this.state.quantity} onChange={this.handleChange} className={ `form-control ${classes.noZoom}` } />
           </label><br/>
-          <label>
+          <label className={ classes.labels }>
             Notes:
-            <textarea type="text" name="notes" value={this.state.notes} onChange={this.handleChange} className="form-control" />
+            <textarea type="text" name="notes" value={this.state.notes} onChange={this.handleChange} className={ `form-control ${classes.noZoom}` } />
           </label><br/>
-          <input type="submit" value="Submit" className="btn btn-primary" />
-          <Link to='/' className='btn btn-primary'>Cancel</Link>
+          <div className={ classes.btnContainer }>
+            <input type="submit" value="Submit" className={ `${classes.btn} ${classes.noZoom}` } />
+            <Link to='/' className={ `${classes.btn} ${classes.btnLink}` }>Cancel</Link>
+          </div>
         </form>
       </div>
     );
   }
 }
 
-export default withRouter(AddItem);
+// export default withRouter(AddItem);
+export default injectSheet(EditItemStyles)(AddItem);
